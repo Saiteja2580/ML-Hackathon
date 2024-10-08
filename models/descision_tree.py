0b594a319fb6aa13ad85588de1ead8c5b86dd2e6
Saiteja2580 <saitejassp2580@gmail.com>
2024-10-08 13:13:31 +0530

    Model Training COmpleted

--- models/descision_tree.py
+++ models/descision_tree.py
@@ -5,10 +5,10 @@ from sklearn.metrics import accuracy_score,confusion_matrix,classification_repor
 
 
 def train_decision_tree_model(x,y):
-    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+    
 
     model = DecisionTreeClassifier(random_state=42)
-    model.fit(x_train,y_train)
+    model.fit(x,y)
 
     
     return model
